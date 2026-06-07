dee3544c77b85002b8abb6854fc0ff5ba325f2ea
loreleysarmiento <sarmientoloreley@gmail.com>
2026-06-07 03:50:49 -0500

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {Login} from './features/auth/pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path: '', component: Login},
+  {path: '**', redirectTo: 'login'},
+];
